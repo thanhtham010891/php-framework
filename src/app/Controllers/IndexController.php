78575cb295fe1652f01a2e6f067f4d47d56feef3
User thamtt <thamtt@nal.vn>
2018-10-02 17:10:23 +0700

    same

--- src/app/Controllers/IndexController.php
+++ src/app/Controllers/IndexController.php
@@ -2,34 +2,16 @@
 
 namespace App\Controllers;
 
-use App\Models\Post;
-use App\Models\User;
-use App\Providers\Database\Model\UseModelTrait;
-
 use System\Controller;
 
 class IndexController extends Controller
 {
 
-    use UseModelTrait;
-
     /**
-     * @throws \System\BaseException
+     * Simple method
      */
     public function index()
     {
-        /**
-         * @var User $user
-         * @var Post $post
-         */
-        $user = $this->getModel(User::class);
-        $post = $this->getModel(Post::class);
-
-        $builder = $user->getQueryBuilder();
-
-        $builder->whereIn('id', [1, 2, 3])->whereEqual('email', '"thamtt@nal.vn"');
-        $builder->innerJoin('posts', $post->getQueryBuilder()->whereEqual('user_id', 'users.id'));
-
-        echo $builder->buildExecuteNoneQuery();
+        echo 'Hello world';
     }
 }
